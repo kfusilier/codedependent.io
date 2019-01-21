e60354919da5a6fa399ac3b170da90068b230007
chris.bridges <christopher_bridges@icloud.com>
2019-01-21 12:58:42 -0500

    slight tweaks to Bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -31,11 +31,11 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
+              Written by <strong>{author}</strong> who loves teaching
+              and building useful things.
               {` `}
               <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
+                Feel free to follow him on Twitter
               </a>
             </p>
           </div>
